1eda4a256dd1a804f4d959e5b1e2d429ed4565ec
robert <dungeonry@hotmail.com>
2021-05-15 18:04:49 -0700

    form validation for title and question as well as minimum choices being two and addressing the crashing that would occur when you would drop below zero choices.

--- components/NewPoll.tsx
+++ components/NewPoll.tsx
@@ -1,52 +1,92 @@
-import React from "react";
+import React, { MouseEvent, FunctionComponent, ChangeEvent, FormEvent } from "react";
 
-export default function NewPoll({
-    onSubmit, 
-    onChoiceChange, 
-    choiceValues, 
-    title, 
-    setTitle, 
-    question, 
-    setQuestion
-}){   
+interface Props {
+    onSubmit: (e: FormEvent<HTMLFormElement>) => void;
+    onChoiceChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    choiceValues: object;
+    title: string;
+    setTitle: (title: string) => void;
+    question: string;
+    setQuestion: (question: string) => void;
+    isQuestionEmpty: boolean;
+    isTitleEmpty: boolean;
+}
 
-    const [inputCount, setInputCount] = React.useState(1);
+
+
+const NewPoll: FunctionComponent<Props> = ({
+    onSubmit,
+    onChoiceChange,
+    choiceValues,
+    title,
+    setTitle,
+    question,
+    setQuestion,
+    isQuestionEmpty,
+    isTitleEmpty
+}) => {
+
+    const [inputCount, setInputCount] = React.useState<number>(2);
+
+    const handleChoiceCountChange = (e: MouseEvent<HTMLButtonElement>) => {
+        const { name } = e.target as HTMLButtonElement;
+        if (name === "increment") {
+            const incrementValue: number = inputCount + 1;
+            setInputCount(incrementValue);
+        }
+
+        if (name === "decrement") {
+            const decrementValue: number = inputCount - 1 >= 2 ? inputCount - 1 : inputCount;
+            setInputCount(decrementValue)
+        }
+
+    }
 
     return (
         <>
-        
-        <form onSubmit = {onSubmit}>
-            <label>Title</label><br/>
-            <input type = "text" 
-                name="title" 
-                value = {title} 
-                onChange={e => setTitle(e.target.value)}/><br/>
-            <label>Question</label><br/>
-            <input type = "text" 
-                name="question" 
-                value = {question} 
-                onChange={e => setQuestion(e.target.value)}/><br/>
-                    
-            {
-                 Array.from(Array(inputCount)).map((_, i) => (
-                    <div key={i}>
-                        <label>Choice {i+1}</label>
-                        <br/>
-                        <input type = "text" 
-                            name={`choice${i+1}`} 
-                            value = {choiceValues[`choice${i+1}`]} 
-                            onChange={e => onChoiceChange(e)} />                            
-                        <br/>
-                    </div>
-                ))
-            }
-            
-            <br/>
-            <button type="button" onClick={()=>setInputCount(inputCount+1)}>Add</button>
-            <button type="button" onClick={()=>setInputCount(inputCount-1)}>Remove Last Input</button>
-            <br/>
-            <input type = "submit" value="Submit"/>
-        </form>
+            <form onSubmit={onSubmit}>
+                <label>Title</label><br />
+                <input type="text"
+                    name="title"
+                    value={title}
+                    onChange={e => setTitle(e.target.value)} /><br />
+
+                {
+                    isTitleEmpty ? <div>Title field cannot be empty.</div> : null
+                }
+
+                <label>Question</label><br />
+                <input type="text"
+                    name="question"
+                    value={question}
+                    onChange={e => setQuestion(e.target.value)} /><br />
+                {
+                    isQuestionEmpty ? <div>Question field cannot be empty.</div> : null
+                }
+
+                {
+                    Array.from(Array(inputCount)).map((_, i) => (
+                        <div key={i}>
+                            <label>Choice {i + 1}</label>
+                            <br />
+                            <input type="text"
+                                name={`choice${i + 1}`}
+                                value={choiceValues[`choice${i + 1}`]}
+                                onChange={e => onChoiceChange(e)} />
+                            <br />
+                        </div>
+                    ))
+                }
+
+                <div>
+                    <button type="button" onClick={handleChoiceCountChange} name="increment">Add Choice</button>
+                    <button type="button" onClick={handleChoiceCountChange} name="decrement">Remove Choice</button>
+                </div>
+                <input type="submit" value="Submit" />
+            </form>
         </>
     )
 }
+
+
+export default NewPoll;
--- components/home.tsx
+++ components/home.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, ChangeEvent, FormEvent } from "react"
 import NewPoll from "./NewPoll";
 import Layout from "./Layout";
 import List from "./List";
@@ -12,9 +12,21 @@ export default function HomePage() {
     const [title, setTitle] = useState<string>("");
     const [question, setQuestion] = useState<string>("");
     const [choiceValues, setChoiceValues] = useState<object>({});
+    const [emptyQuestionWarning, setEmptyQuestionWarning] = useState<boolean>(false);
+    const [emptyTitleWarning, setEmptyTitleWarning] = useState<boolean>(false);
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+
+        if (title === "") {
+            setEmptyTitleWarning(true);
+            return;
+        }
+
+        if (title === "" || question === "") {
+            setEmptyQuestionWarning(true);
+            return;
+        }
 
         const payload = {
             title,
@@ -23,6 +35,8 @@ export default function HomePage() {
         }
         setTitle("");
         setQuestion("");
+        setEmptyQuestionWarning(false);
+        setEmptyTitleWarning(false);
         setChoiceValues(() => {
             const update = {};
             Object.keys(choiceValues).forEach(cv => {
@@ -40,7 +54,7 @@ export default function HomePage() {
             });
     }
 
-    const handleChoices = (e) => {
+    const handleChoices = (e: ChangeEvent<HTMLInputElement>) => {
         const { value, name } = e.target;
 
         const update = {};
@@ -52,7 +66,7 @@ export default function HomePage() {
 
 
     useEffect(() => {
-        let mounted = true;
+        let mounted: boolean = true;
         getActivePolls()
             .then(res => {
                 if (mounted) {
@@ -76,6 +90,8 @@ export default function HomePage() {
                 title={title}
                 setTitle={setTitle}
                 question={question}
+                isQuestionEmpty={emptyQuestionWarning}
+                isTitleEmpty={emptyTitleWarning}
                 setQuestion={setQuestion} />
             <List data={data} />
         </Layout>
